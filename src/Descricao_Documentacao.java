b8934e8bbfd992a7a708efc54fdb5047648db3f1
Pedro Alessandro <pedro.paz13@hotmail.com>
2023-04-25 18:38:41 -0300

    Criando uma classe principal e descrevendo documentacao

--- src/Descricao_Documentacao.java
+++ src/Descricao_Documentacao.java
@@ -1,4 +1,47 @@
 
 public class Descricao_Documentacao {
+	
+	//DOCUMENTAÇÃO
+	/**
+	 * 
+	 * Uma das maiores características da linguagem Java é que, desde suas primeiras versões,
+	 *  tínhamos em nossas mãos, uma documentação rica e detalhada dos 
+	 *  recursos da linguagem.
+	 *	Conforme site oficial, podemos compreender e explorar, todos os recursos 
+	 *	organizados por pacotes e classes bem específicas, 
+	 *	sem nem mesmo escrever uma linha de código.
+	 *	Hoje, costuma-se afirmar que, para se tornar um desenvolvedor 
+	 *	nível avançado, é um requisito imprescindível adquirir a 
+	 *	habilidade de compreender, a documentação oficial da linguagem 
+	 *	e dos frameworks que são incorporados nos projetos atuais.
+	 *	Aqui, temos o link da documentação de uma das principais classes
+	 *	 da linguagem Java:
+	 * 
+	 */
 
+	//TAGS
+		/**
+		 * 
+		 * Mas e quais as informações, que obtemos através de classes 
+		 * documentadas, na linguagem ? Java Documentation é composto 
+		 * por tags que, representam dados relevantes para a compreensão 
+		 * da proposta de uma classe e os conjuntos de seus métodos e atributos 
+		 * conforme tabela abaixo:
+		 * 
+		 */
+	
+		/**
+		 * 
+		 * TAG					DESCRICAO
+		 * @Author     			Autor/criador
+		 * @version				Versão de recurso disponibilizado
+		 * @since				Versão/Data de inicio da disponibilização do recurso
+		 * @param				Descrição dos parametros dos metodos criados
+		 * @return				Definicao do tipo de retorno do metodo
+		 * @throws				Se o metodo lanca alguma exceção
+		 * 
+		 */
+	
+	
+		
 }
